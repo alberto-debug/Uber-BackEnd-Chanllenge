45e002836e2187243449360869558543bb2ac578
Alberto-debug <albertojunior848423803@gmail.com>
2026-01-27 15:19:44 +0300

    changes

--- src/main/java/com/alberto/email_service/infra/ses/AwsSesConfig.java
+++ src/main/java/com/alberto/email_service/infra/ses/AwsSesConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AwsSesConfig {
 
+
     @Bean
     public AmazonSimpleEmailService amazonSimpleEmailService(){
         return AmazonSimpleEmailServiceClientBuilder.standard().build();
